bc195f1def45a307154f703340d68d11afac7986
Esa-Matti Suuronen <code@esamatti.fi>
2026-03-21 22:24:07 +0200

    faster playwright startup

--- ui/playwright.config.ts
+++ ui/playwright.config.ts
@@ -72,7 +72,7 @@ export default defineConfig({
 
     /* Run your local dev server before starting the tests */
     webServer: {
-        command: "../scripts/dev",
+        command: "../scripts/test/playwright-dev",
         url: "http://localhost:4000/",
         reuseExistingServer: !process.env.CI,
     },
